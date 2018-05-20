4ed77a03d89df4b8db9b97823336a5ae7f2a0204
mratsim <mamy_github@numforge.co>
2018-05-20 10:27:30 +0200

    Add external source as input

--- m110_feat_extraction_pipeline.py
+++ m110_feat_extraction_pipeline.py
@@ -6,6 +6,7 @@ from src.feature_engineering.fte_cyclic_time import fte_cyclic_time
 from src.feature_engineering.fte_age import fte_age
 from src.feature_engineering.fte_employment import fte_organisation
 from src.feature_engineering.fte_money_bureau import fte_prev_credit_situation
+from src.feature_extraction.fte_ext_source import fte_ext_source
 
 pipe_transforms = feat_extraction_pipe(
   fte_income_ratios,
@@ -13,5 +14,6 @@ pipe_transforms = feat_extraction_pipe(
   fte_goods_price,
   fte_age,
   fte_organisation,
-  fte_prev_credit_situation
+  fte_prev_credit_situation,
+  fte_ext_source
 )
