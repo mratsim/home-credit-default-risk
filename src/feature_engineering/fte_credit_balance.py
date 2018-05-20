e6f218637b72887f7894f0a00fdea52c24379d1a
mratsim <mamy_github@numforge.co>
2018-05-20 21:24:40 +0200

    val0.77772 - Add credit card balance

---
+++ src/feature_engineering/fte_credit_balance.py
@@ -0,0 +1,42 @@
+# Copyright 2018 Mamy André-Ratsimbazafy. All rights reserved.
+
+import pandas as pd
+
+def fte_withdrawals(train, test, y, db_conn, folds, cache_file):
+  def _trans(df, table):
+    query = f"""
+    select
+      avg(CNT_DRAWINGS_ATM_CURRENT) as cb_avg_atm_withdrawal_count,
+      avg(AMT_DRAWINGS_ATM_CURRENT) as cb_avg_atm_withdrawal_amount,
+      avg(CNT_DRAWINGS_CURRENT) as cb_avg_withdrawal_count,
+      avg(AMT_DRAWINGS_CURRENT) as cb_avg_withdrawal_amount,
+      avg(CNT_DRAWINGS_POS_CURRENT) as cb_avg_pos_withdrawal_count,
+      avg(AMT_DRAWINGS_POS_CURRENT) as cb_avg_pos_withdrawal_amount,
+      avg(SK_DPD) as cb_avg_day_past_due
+    FROM
+      {table} app
+    left join
+      credit_card_balance ccb
+        on app.SK_ID_CURR = ccb.SK_ID_CURR
+    -- where
+    --  MONTHS_BALANCE BETWEEN -3 and -1
+    group by
+      ccb.sk_id_curr
+    ORDER by
+      app.SK_ID_CURR
+    """
+
+    df[[
+      'cb_avg_atm_withdrawal_count',
+      'cb_avg_atm_withdrawal_amount',
+      'cb_avg_withdrawal_count',
+      'cb_avg_withdrawal_amount',
+      'cb_avg_pos_withdrawal_count',
+      'cb_avg_pos_withdrawal_amount',
+      'cb_avg_day_past_due',
+      ]] = pd.read_sql_query(query, db_conn)
+
+  _trans(train, "application_train")
+  _trans(test, "application_test")
+
+  return train, test, y, db_conn, folds, cache_file
