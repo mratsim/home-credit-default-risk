b83b667a968216559e8c06a3db15e0e76214529c
mratsim <mamy_github@numforge.co>
2018-05-20 10:15:05 +0200

    Add active credit information

--- src/feature_engineering/fte_money_bureau.py
+++ src/feature_engineering/fte_money_bureau.py
@@ -6,6 +6,8 @@ def fte_prev_credit_situation(train, test, y, db_conn, folds, cache_file):
   def _trans(df, table):
     query = f"""
     select
+      IFNULL(count(b.SK_ID_CURR), 0) AS total_prev_applications,
+      IFNULL(sum(case CREDIT_ACTIVE when 'Active' then 1 else 0 end), 0) AS current_active_applications,
       IFNULL(sum(AMT_CREDIT_SUM), 0) AS total_prev_credit,
       IFNULL(sum(case CREDIT_ACTIVE when 'Active' then AMT_CREDIT_SUM else 0 end), 0) AS active_credit_amount,
       IFNULL(sum(AMT_CREDIT_SUM_DEBT), 0) AS current_debt
@@ -19,7 +21,7 @@ def fte_prev_credit_situation(train, test, y, db_conn, folds, cache_file):
       app.SK_ID_CURR ASC
     """
 
-    df[['total_prev_credit', 'active_credit_amount', 'current_debt']] = pd.read_sql_query(query, db_conn)
+    df[['total_prev_applications','current_active_applications','total_prev_credit', 'active_credit_amount', 'current_debt']] = pd.read_sql_query(query, db_conn)
 
     # TODO add currency, otherwise credit is not comparable
 
