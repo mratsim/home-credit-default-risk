03f7d012dbfc21c85f2dd0bf95f22d6079b0950f
mratsim <mamy_github@numforge.co>
2018-05-20 12:39:36 +0200

    Fix bureau prediction not joining "application_test" + Add DAYS_CREDIT predictions

--- src/feature_engineering/fte_money_bureau.py
+++ src/feature_engineering/fte_money_bureau.py
@@ -12,14 +12,16 @@ def fte_prev_credit_situation(train, test, y, db_conn, folds, cache_file):
       IFNULL(sum(case CREDIT_ACTIVE when 'Active' then AMT_CREDIT_SUM else 0 end), 0) AS active_credit_amount,
       IFNULL(sum(AMT_CREDIT_SUM_DEBT), 0) AS current_debt,
       IFNULL(sum(AMT_CREDIT_MAX_OVERDUE), 0) AS current_overdue,
+      IFNULL(max(-DAYS_CREDIT / 365.25), 99) AS first_credit_years_ago,
+      IFNULL(min(-DAYS_CREDIT / 365.25), 99) AS last_credit_years_ago
     from
-      application_train app
+      {table} app
     left join
       bureau b on app.SK_ID_CURR = b.SK_ID_CURR
     GROUP BY
       app.SK_ID_CURR
     ORDER BY
-      app.SK_ID_CURR ASC
+      app.SK_ID_CURR ASC;
     """
 
     df[['total_prev_applications',
@@ -27,7 +29,9 @@ def fte_prev_credit_situation(train, test, y, db_conn, folds, cache_file):
         'total_prev_credit',
         'active_credit_amount',
         'current_debt',
-        'current_overdue']] = pd.read_sql_query(query, db_conn)
+        'current_overdue',
+        'first_credit_years_ago',
+        'last_credit_years_ago']] = pd.read_sql_query(query, db_conn)
 
     # TODO add currency, otherwise credit is not comparable
 
