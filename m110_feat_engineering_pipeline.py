f639c33fc2caefa2d0f075388c5b8eca618378dd
mratsim <mamy_github@numforge.co>
2018-05-21 17:20:46 +0200

    add potentially leaky IDs

--- m110_feat_engineering_pipeline.py
+++ m110_feat_engineering_pipeline.py
@@ -16,6 +16,7 @@ from src.feature_extraction.fte_region import fte_region
 from src.feature_extraction.fte_building import fte_building
 from src.feature_extraction.fte_social_circle_default import fte_social_circle_default
 from src.feature_extraction.fte_application_process import fte_application_process, fte_contract
+from src.feature_extraction.fte_magic_leaks import fte_magic_ids_leak
 
 pipe_transforms = feat_engineering_pipe(
   fte_income_ratios,
---
+++ src/feature_extraction/fte_magic_leaks.py
@@ -0,0 +1,33 @@
+# Copyright 2018 Mamy André-Ratsimbazafy. All rights reserved.
+
+import pandas as pd
+
+# A collection of features that predict by proxy
+# but are not supposed to have any predictive power
+
+def fte_magic_ids_leak(train, test, y, db_conn, folds, cache_file):
+  def _trans(df, table):
+    query = f"""
+    select
+      avg(b.SK_ID_BUREAU) as avg_magic_id_bureau,
+      avg(p.SK_ID_PREV) as avg_magic_id_prev
+    from
+      {table} app
+    left join
+      bureau b on app.SK_ID_CURR = b.SK_ID_CURR
+    left join
+      previous_application p on app.SK_ID_CURR = p.SK_ID_CURR
+    group by
+      app.SK_ID_CURR
+    order by
+      app.SK_ID_CURR ASC
+    """
+
+    df[['avg_magic_id_bureau',
+        'avg_magic_id_prev'
+        ]] = pd.read_sql_query(query, db_conn)
+
+  _trans(train, "application_train")
+  _trans(test, "application_test")
+
+  return train, test, y, db_conn, folds, cache_file
