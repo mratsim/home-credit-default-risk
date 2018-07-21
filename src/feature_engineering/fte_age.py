7d8e8c61582740c1001d4f04dbe5993cecded87d
mratsim <mamy_github@numforge.co>
2018-07-21 17:53:34 +0200

    Add several age and income ratios

--- src/feature_engineering/fte_age.py
+++ src/feature_engineering/fte_age.py
@@ -7,23 +7,57 @@ from src.instrumentation import logspeed
 def fte_age(train, test, y, db_conn, folds, cache_file):
   def _trans(df, table):
     query = f"""
+    with raw_age_query AS (
+      select
+        SK_ID_CURR,
+        -DAYS_BIRTH / 365.25 AS years_birth,
+        -DAYS_EMPLOYED / 365.25 AS years_employed,
+        -DAYS_REGISTRATION / 365.25 AS years_registration,
+        -DAYS_ID_PUBLISH / 365.25 AS years_id_publish,
+        case DAYS_LAST_PHONE_CHANGE
+          when 0 then NULL
+          else -DAYS_LAST_PHONE_CHANGE / 365.25
+        end years_last_phone_change,
+        OWN_CAR_AGE,
+        CNT_FAM_MEMBERS - CNT_CHILDREN AS count_adults
+      from
+        {table}
+      order by
+        SK_ID_CURR ASC
+      )
     select
-      -DAYS_BIRTH / 365.25 AS years_birth,
-      -DAYS_EMPLOYED / 365.25 AS years_employed,
-      -DAYS_REGISTRATION / 365.25 AS years_registration,
-      -DAYS_ID_PUBLISH / 365.25 AS years_id_publish,
-      case DAYS_LAST_PHONE_CHANGE
-        when 0 then NULL
-        else -DAYS_LAST_PHONE_CHANGE / 365.25
-      end years_last_phone_change,
-      OWN_CAR_AGE
+      years_birth,
+      years_employed,
+      years_registration,
+      years_id_publish,
+      years_last_phone_change,
+      OWN_CAR_AGE,
+      count_adults,
+      OWN_CAR_AGE / years_birth AS car_age_birth_ratio,
+      OWN_CAR_AGE / years_employed AS car_age_employment_ratio,
+      years_employed / years_birth AS employment_birth_ratio,
+      years_last_phone_change / years_birth AS phone_age_birth_ratio,
+      years_last_phone_change / years_employed AS phone_age_employment_ratio
     from
-      {table}
+      raw_age_query
     order by
-      SK_ID_CURR ASC;
+      SK_ID_CURR ASC
     """
 
-    df[['years_birth', 'years_employed', 'years_registration', 'years_id_publish', 'years_last_phone_change', 'OWN_CAR_AGE']] = pd.read_sql_query(query, db_conn)
+    df[[
+      'years_birth',
+      'years_employed',
+      'years_registration',
+      'years_id_publish',
+      'years_last_phone_change',
+      'OWN_CAR_AGE',
+      'count_adults',
+      'car_age_birth_ratio',
+      'car_age_employment_ratio',
+      'employment_birth_ratio',
+      'phone_age_birth_ratio',
+      'phone_age_employment_ratio'
+      ]] = pd.read_sql_query(query, db_conn)
 
   _trans(train, "application_train")
   _trans(test, "application_test")
--- src/feature_engineering/fte_money.py
+++ src/feature_engineering/fte_money.py
@@ -10,6 +10,14 @@ def fte_income_ratios(train, test, y, db_conn, folds, cache_file):
     select
       AMT_CREDIT / AMT_INCOME_TOTAL AS credit_income_ratio,
       AMT_ANNUITY / AMT_INCOME_TOTAL AS annuity_income_ratio,
+      AMT_CREDIT / AMT_ANNUITY AS credit_annuity_ratio,
+      AMT_CREDIT / AMT_GOODS_PRICE as credit_price_ratio,
+      AMT_INCOME_TOTAL / CNT_CHILDREN AS income_per_child, --If no children, div by 0, infinite income
+      AMT_INCOME_TOTAL / CNT_FAM_MEMBERS AS income_per_person,
+      AMT_INCOME_TOTAL / (CNT_FAM_MEMBERS - CNT_CHILDREN) AS income_per_adult,
+      AMT_CREDIT / CNT_CHILDREN AS credit_per_child, --If no children, div by 0, infinite income
+      AMT_CREDIT / CNT_FAM_MEMBERS AS credit_per_person,
+      AMT_CREDIT / (CNT_FAM_MEMBERS - CNT_CHILDREN) AS credit_per_adult,
       AMT_CREDIT,
       AMT_INCOME_TOTAL,
       AMT_ANNUITY
@@ -19,7 +27,20 @@ def fte_income_ratios(train, test, y, db_conn, folds, cache_file):
       SK_ID_CURR ASC;
     """
 
-    df[['credit_income_ratio', 'annuity_income_ratio', 'AMT_CREDIT', 'AMT_INCOME_TOTAL', 'AMT_ANNUITY']] = pd.read_sql_query(query, db_conn)
+    df[[
+      'credit_income_ratio',
+      'annuity_income_ratio',
+      'credit_annuity_ratio',
+      'credit_price_ratio',
+      'income_per_child',
+      'income_per_person',
+      'income_per_adult',
+      'credit_per_child',
+      'credit_per_person',
+      'credit_per_adult',
+      'AMT_CREDIT',
+      'AMT_INCOME_TOTAL',
+      'AMT_ANNUITY']] = pd.read_sql_query(query, db_conn)
 
   _trans(train, "application_train")
   _trans(test, "application_test")
